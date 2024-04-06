fe3f1cbf235f1b7c8e58eb957d530398566e7f5c
Morten Lunde <morten.lunde@live.no>
2024-04-07 01:36:11 +0200

    Updated with new solution name, added 2nd project

---
+++ OppgaverLambdaExpression/Program.cs
@@ -0,0 +1,5 @@
+﻿// Oppgave 1
+static IEnumerable<int> NumbersBiggerThanTen(int[] numbers) => numbers.ToList().Where(num => num > 10);
+Console.WriteLine(string.Join(", ", NumbersBiggerThanTen([1, 7, 99, 20, 11, 18, 5])));
+
+// Oppgave 2
